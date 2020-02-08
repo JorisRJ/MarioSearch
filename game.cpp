4f3f8bfeac81e9d2963712b5b83fab8667704635
Joris L <Joris556@outlook.com>
2020-02-08 13:40:30 +0100

    random

--- game.cpp
+++ game.cpp
@@ -7,7 +7,7 @@
 //https://www.reddit.com/r/generative/comments/ez9q65/how_to_transform_a_painting_into_a_particle_field/?utm_source=share&utm_medium=web2x
 // -----------------------------------------------------------
 
-static Sprite theImage( new Surface( "assets/123.jpg" ), 1 );
+static Sprite theImage( new Surface( "assets/jhv.jpg" ), 1 );
 constexpr int PARTICLES = 100000;
 constexpr float fade = 31.f / 32.f;
 Surface *mainImage;
@@ -103,8 +103,11 @@ void Game::Tick( float deltaTime )
 		particles[i].x += particles[i].dx + 0.05f;
 		particles[i].y += particles[i].dy;
 
-		if ( particles[i].x > SCRWIDTH )
+		if (particles[i].x > SCRWIDTH)
+		{
 			particles[i].x = 0;
+			particles[i].y = XorShift() % SCRHEIGHT;
+		}
 
 		p[(int)( particles[i].x + particles[i].y * SCRWIDTH )] = lum[(int)( particles[i].x + particles[i].y * SCRWIDTH )];
 	}
